f7b311328e867f2d643b15e5f0856e0441644c7e
Callum Barker <64287849+cbarkr@users.noreply.github.com>
2022-06-06 14:34:35 -0700

    Create Pangrams.js

---
+++ HackerRank/Pangrams.js
@@ -0,0 +1,24 @@
+/*
+ * Complete the 'pangrams' function below.
+ *
+ * The function is expected to return a STRING.
+ * The function accepts STRING s as parameter.
+ */
+
+function pangrams(s) {
+    // Write your code here
+    
+    // Remove spaces and convert to lower case
+    s = s.toLowerCase().trim().replace(/ /g, "")
+    
+    let map = new Map()
+    
+    for (let i = 0; i < s.length; i++){
+        let count = map.get(s[i])
+        map.set(s[i], count !== undefined ? count+1 : 1)
+    }
+    
+    if (map.size !== 26){ return "not pangram" }
+    
+    return "pangram"
+}
