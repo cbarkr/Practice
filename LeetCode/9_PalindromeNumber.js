deb0fe279d03eb16cd7a7189a2617bfb05b96e80
Callum Barker <64287849+cbarkr@users.noreply.github.com>
2022-06-13 16:33:03 -0700

    Update 9_PalindromeNumber.js

--- LeetCode/9_PalindromeNumber.js
+++ LeetCode/9_PalindromeNumber.js
@@ -3,18 +3,5 @@
  * @return {boolean}
  */
 var isPalindrome = function(x) {
-    let xString = x.toString()
-    let xStringLen = xString.length
-    
-    let first = xString.slice(0, xStringLen / 2)
-    
-    // Even length
-    if (xStringLen % 2 === 0){
-        let second = xString.slice(xStringLen / 2)
-        return first === second.split('').reverse().join('')
-    }
-
-    // Odd length
-    let second = xString.slice(xStringLen / 2 + 1)
-    return first === second.split('').reverse().join('')
+    return x.toString() === x.toString().split('').reverse().join('')
 };
