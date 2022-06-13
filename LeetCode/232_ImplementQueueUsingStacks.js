c9ab2e0f9d77db1e9d803b7dedf9d58eb6786d33
Callum Barker <64287849+cbarkr@users.noreply.github.com>
2022-06-13 13:30:37 -0700

    Create 232_ImplementQueueUsingStacks.js

---
+++ LeetCode/232_ImplementQueueUsingStacks.js
@@ -0,0 +1,48 @@
+
+var MyQueue = function() {
+    this.stack = []
+};
+
+/** 
+ * @param {number} x
+ * @return {void}
+ */
+MyQueue.prototype.push = function(x) {
+    this.stack.unshift(x)
+};
+
+/**
+ * @return {number}
+ */
+MyQueue.prototype.pop = function() {
+    if (this.stack.length === 0){ return null }
+    
+    return this.stack.pop()
+};
+
+/**
+ * @return {number}
+ */
+MyQueue.prototype.peek = function() {  
+    if (this.stack.length === 0){ return null }
+    
+    return this.stack[this.stack.length-1]
+};
+
+/**
+ * @return {boolean}
+ */
+MyQueue.prototype.empty = function() {
+    if (this.stack.length === 0){ return true }
+    
+    return false
+};
+
+/** 
+ * Your MyQueue object will be instantiated and called as such:
+ * var obj = new MyQueue()
+ * obj.push(x)
+ * var param_2 = obj.pop()
+ * var param_3 = obj.peek()
+ * var param_4 = obj.empty()
+ */
