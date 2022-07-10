cc6bc627fccd1b67ad4b5408e6a7c310369f2d38
Callum Barker <64287849+cbarkr@users.noreply.github.com>
2022-07-10 14:54:02 -0700

    Create SherlockAndValidString.js

---
+++ HackerRank/SherlockAndValidString.js
@@ -0,0 +1,31 @@
+/*
+ * Complete the 'isValid' function below.
+ *
+ * The function is expected to return a STRING.
+ * The function accepts STRING s as parameter.
+ */
+
+function isValid(s) {
+    // Write your code here
+    let map = new Map()
+    let commonFreq = 0, commonCount = 0
+    let isValid = true
+    
+    // Populate frequency map
+    for (let i = 0; i < s.length; i++){
+        let count = map.get(s[i])
+        map.set(s[i], count === undefined ? 1 : count+1)
+    }
+    
+    // Get count of first element in string
+    commonFreq = map.get(s[0])
+    
+    // Iterate over counts, check if equal
+    map.forEach(
+        (value, key) => {
+            if (value === commonFreq){ commonCount++ }
+        }
+    )
+    
+    return (commonCount === map.size || commonCount === (map.size -1)) ? "YES" : "NO"
+}
