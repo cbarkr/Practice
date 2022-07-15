c2917a42658169d8e915f3c649172b6fe313e147
Callum Barker <64287849+cbarkr@users.noreply.github.com>
2022-07-15 13:23:03 -0700

    Update 1_TwoSum.js

--- LeetCode/1_TwoSum.js
+++ LeetCode/1_TwoSum.js
@@ -4,15 +4,12 @@
  * @return {number[]}
  */
 var twoSum = function(nums, target) {
-    let result = [];
-    
     for (let i = 0; i < nums.length - 1; i++){
         for (let j = i + 1; j < nums.length; j++){
             if ((target - nums[i]) == nums[j]){
-                result.push(i);
-                result.push(j);
+                return [i, j]
             }
         }
     }
-    return result;
+    return []
 };
