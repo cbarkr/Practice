b3a452574af2ec4a20240e17dada3325d2f55ec2
Callum Barker <64287849+cbarkr@users.noreply.github.com>
2022-07-21 20:58:06 -0700

    Create 1_TwoSum_altII.js

---
+++ LeetCode/1_TwoSum_altII.js
@@ -0,0 +1,19 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ */
+var twoSum = function(nums, target) {
+    let numsMap = new Map()
+    
+    for (let i = 0; i < nums.length; i++){
+        let count = numsMap.get(target - nums[i])
+        if (count !== undefined){
+            return [nums.indexOf(target-nums[i]), i]
+        }
+        
+        numsMap.set(nums[i], count+1)
+    }
+    
+    return []
+};
