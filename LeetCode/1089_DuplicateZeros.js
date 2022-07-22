b4f1104b93d156c41241f57e4c938ed7f15878a3
Callum Barker <64287849+cbarkr@users.noreply.github.com>
2022-07-21 22:09:39 -0700

    Create 1089_DuplicateZeros.js

---
+++ LeetCode/1089_DuplicateZeros.js
@@ -0,0 +1,21 @@
+/**
+ * @param {number[]} arr
+ * @return {void} Do not return anything, modify arr in-place instead.
+ */
+var duplicateZeros = function(arr) {
+    if (arr.indexOf(0) === -1){ return; }
+    
+    let i = 0, dupCount = 0
+    
+    while (i < arr.length){
+        if (arr[i] === 0){
+            arr.splice(i, 0, 0)
+            i++
+            dupCount++
+        }
+        
+        i++
+    }
+    
+    arr.splice(arr.length - dupCount, dupCount)
+};
