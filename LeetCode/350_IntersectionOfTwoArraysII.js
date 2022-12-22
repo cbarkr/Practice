9b2f48607573e739d1a3e1fcd888832d99e1434b
Callum Barker <64287849+cbarkr@users.noreply.github.com>
2022-12-21 16:43:09 -0800

    Update 350_IntersectionOfTwoArraysII.js

--- LeetCode/350_IntersectionOfTwoArraysII.js
+++ LeetCode/350_IntersectionOfTwoArraysII.js
@@ -9,12 +9,12 @@ var intersect = function(nums1, nums2) {
     
     for (let i = 0; i < nums1.length; i++){
         let numsCount = numsMap.get(nums1[i])
-        numsMap.set(nums1[i], numsCount === undefined ? 1 : numsCount+1)
+        numsMap.set(nums1[i], !numsCount ? 1 : numsCount+1)
     }
     
     for (let j = 0; j < nums2.length; j++){
         let numsCount = numsMap.get(nums2[j])
-        if (numsCount !== undefined && numsCount > 0){
+        if (numsCount && numsCount > 0){
             intersection.push(nums2[j])
             numsMap.set(nums2[j], numsCount-1)
         }
